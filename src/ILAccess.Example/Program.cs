c0aedcfa130f970ffb073597ccc753ba66fae831
huoshan12345 <huoshan12345@hotmail.com>
2025-10-22 11:42:36 -0230

    refine

--- src/ILAccess.Example/Program.cs
+++ src/ILAccess.Example/Program.cs
@@ -1,38 +1,126 @@
 ﻿using System;
-using System.Diagnostics.CodeAnalysis;
-#pragma warning disable CA2211
+using System.Runtime.CompilerServices;
+// ReSharper disable UnusedMember.Local
+// ReSharper disable FieldCanBeMadeReadOnly.Local
+// ReSharper disable InconsistentNaming
+// ReSharper disable ConvertToAutoProperty
+// ReSharper disable ArrangeAccessorOwnerBody
+// ReSharper disable UnusedParameter.Local
+// ReSharper disable UnusedMember.Global
+// ReSharper disable UnusedVariable
+#pragma warning disable CS0169 // Field is never used
+#pragma warning disable IDE0044 // Add readonly modifier
 #pragma warning disable IDE0051
+#pragma warning disable IDE0059 // Unnecessary assignment of a value
+#pragma warning disable CA1822 // Mark members as static
+#pragma warning disable IDE0060 // Remove unused parameter
 
 namespace ILAccess.Example;
 
-[SuppressMessage("ReSharper", "UnusedMember.Global")]
-[SuppressMessage("ReSharper", "UnusedMember.Local")]
-public class TestModel
+public class Class
 {
-    public int PublicProperty { get; set; } = 1;
-    public static int PublicStaticProperty { get; set; } = 1;
+    private static void StaticPrivateMethod() { }
+    private static int StaticPrivateField;
+    private Class(int i) { PrivateField = i; }
+    private void PrivateMethod() { }
+    private int PrivateField;
+    private int PrivateProperty { get => PrivateField; }
+}
 
-    private static int PrivateStaticProperty { get; set; } = 1;
-    private int PrivateProperty { get; set; } = 1;
+// Generic example
+public class Class<T>
+{
+    private T? _field;
+    private void M(T t) { }
+    private void GM<U>(U u) { }
+    private void GMWithConstraints<U, V>(U u, V v) where U : V, IEquatable<U> { }
+}
 
-    public static int PublicStaticField = 1;
-    public int PublicField = 1;
+internal class Accessors<V>
+{
+    [UnsafeAccessor(UnsafeAccessorKind.Field, Name = "_field")]
+    public static extern ref V GetSetPrivateField(Class<V> c);
+
+    [UnsafeAccessor(UnsafeAccessorKind.Method, Name = "M")]
+    public static extern void CallM(Class<V> c, V v);
+
+    [UnsafeAccessor(UnsafeAccessorKind.Method, Name = "GM")]
+    public static extern void CallGM<X>(Class<V> c, X x);
+
+    [UnsafeAccessor(UnsafeAccessorKind.Method, Name = "GMWithConstraints")]
+    public static extern void CallGMWithConstraints<X, Y>(Class<V> c, X x, Y y) where X : Y, IEquatable<X>;
 }
 
 internal class Program
 {
+    public static void CallStaticPrivateMethod()
+    {
+        StaticPrivateMethod(null);
+
+        [UnsafeAccessor(UnsafeAccessorKind.StaticMethod, Name = nameof(StaticPrivateMethod))]
+        static extern void StaticPrivateMethod(Class? c);
+    }
+    public static void GetSetStaticPrivateField()
+    {
+        ref var f = ref GetSetStaticPrivateField(null);
+
+        [UnsafeAccessor(UnsafeAccessorKind.StaticField, Name = "StaticPrivateField")]
+        static extern ref int GetSetStaticPrivateField(Class? c);
+    }
+    public static void CallPrivateConstructor()
+    {
+        var c1 = PrivateCtor(1);
+        var c2 = (Class)RuntimeHelpers.GetUninitializedObject(typeof(Class));
+        PrivateCtorAsMethod(c2, 2);
+
+        [UnsafeAccessor(UnsafeAccessorKind.Constructor)]
+        static extern Class PrivateCtor(int i);
+
+        [UnsafeAccessor(UnsafeAccessorKind.Method, Name = ".ctor")]
+        static extern void PrivateCtorAsMethod(Class c, int i);
+
+    }
+    public static void CallPrivateMethod(Class c)
+    {
+        PrivateMethod(c);
+
+        [UnsafeAccessor(UnsafeAccessorKind.Method, Name = nameof(PrivateMethod))]
+        static extern void PrivateMethod(Class c);
+    }
+    public static void GetPrivateProperty(Class c)
+    {
+        var f = GetPrivateProperty(c);
+
+        [UnsafeAccessor(UnsafeAccessorKind.Method, Name = "get_PrivateProperty")]
+        static extern int GetPrivateProperty(Class c);
+    }
+    public static void GetSetPrivateField(Class c)
+    {
+        ref var f = ref GetSetPrivateField(c);
+
+        [UnsafeAccessor(UnsafeAccessorKind.Field, Name = "PrivateField")]
+        static extern ref int GetSetPrivateField(Class c);
+    }
+
+    public static void AccessGenericType(Class<int> c)
+    {
+        ref var f = ref Accessors<int>.GetSetPrivateField(c);
+        Accessors<int>.CallM(c, 1);
+        Accessors<int>.CallGM<string>(c, string.Empty);
+        Accessors<int>.CallGMWithConstraints<string, object>(c, string.Empty, new object());
+    }
+
     private static void Main(string[] args)
     {
-        var obj = new TestModel();
+        CallStaticPrivateMethod();
+        GetSetStaticPrivateField();
+        CallPrivateConstructor();
 
-        {
-            var value = obj.ILAccess().GetValue<int>("PrivateProperty");
-            Console.WriteLine("PrivateProperty: " + value);
-        }
-        {
-            var value = obj.ILAccess().GetValue<int>("PrivateStaticProperty");
-            Console.WriteLine("PrivateStaticProperty: " + value);
-        }
+        var c = Activator.CreateInstance<Class>();
+        CallPrivateMethod(c);
+        GetPrivateProperty(c);
+        GetSetPrivateField(c);
+        AccessGenericType(new Class<int>());
 
         Console.Read();
     }
--- src/ILAccess/ILAccessAttribute.cs
+++ src/ILAccess/ILAccessAttribute.cs
@@ -1,8 +1,104 @@
 ﻿namespace ILAccess;
 
 /// <summary>
-/// 
+/// Specifies the kind of target to which an <see cref="ILAccessAttribute" /> is providing access.
 /// </summary>
-public class ILAccessAttribute : Attribute
+public enum ILAccessorKind
 {
+    /// <summary>
+    /// Provide access to a constructor.
+    /// </summary>
+    Constructor,
+
+    /// <summary>
+    /// Provide access to a method.
+    /// </summary>
+    Method,
+
+    /// <summary>
+    /// Provide access to a static method.
+    /// </summary>
+    StaticMethod,
+
+    /// <summary>
+    /// Provide access to a field.
+    /// </summary>
+    Field,
+
+    /// <summary>
+    /// Provide access to a static field.
+    /// </summary>
+    StaticField
+}
+
+/// <summary>
+/// Provides access to an inaccessible member of a specific type.
+/// </summary>
+/// <remarks>
+/// This attribute may be applied to an <code>extern static</code> method.
+/// The implementation of the <code>extern static</code> method annotated with
+/// this attribute will be provided by the runtime based on the information in
+/// the attribute and the signature of the method that the attribute is applied to.
+/// The runtime will try to find the matching method or field and forward the call
+/// to it. If the matching method or field is not found, the body of the <code>extern</code>
+/// method will throw <see cref="MissingFieldException" /> or <see cref="MissingMethodException" />.
+/// Only the specific type defined will be examined for inaccessible members. The type hierarchy
+/// is not walked looking for a match.
+///
+/// For <see cref="ILAccessorKind.Method"/>, <see cref="ILAccessorKind.StaticMethod"/>,
+/// <see cref="ILAccessorKind.Field"/>, and <see cref="ILAccessorKind.StaticField"/>, the type of
+/// the first argument of the annotated <code>extern</code> method identifies the owning type.
+/// The value of the first argument is treated as <code>this</code> pointer for instance fields and methods.
+/// The first argument must be passed as <code>ref</code> for instance fields and methods on structs.
+/// The value of the first argument is not used by the implementation for <code>static</code> fields and methods.
+///
+/// Return type is considered for the signature match. modreqs and modopts are initially not considered for
+/// the signature match. However, if an ambiguity exists ignoring modreqs and modopts, a precise match
+/// is attempted. If an ambiguity still exists <see cref="System.Reflection.AmbiguousMatchException" /> is thrown.
+///
+/// By default, the attributed method's name dictates the name of the method/field. This can cause confusion
+/// in some cases since language abstractions, like C# local functions, generate mangled IL names. The
+/// solution to this is to use the <code>nameof</code> mechanism and define the <see cref="Name"/> property.
+///
+/// <code>
+/// public void Method(Class c)
+/// {
+///     PrivateMethod(c);
+///
+///     [UnsafeAccessor(UnsafeAccessorKind.Method, Name = nameof(PrivateMethod))]
+///     extern static void PrivateMethod(Class c);
+/// }
+/// </code>
+/// </remarks>
+[AttributeUsage(AttributeTargets.Method, AllowMultiple = false, Inherited = false)]
+public sealed class ILAccessAttribute : Attribute
+{
+    // Block of text to include above when Generics support is added:
+    //
+    // The generic parameters of the <code>extern static</code> method are a concatenation of the type and
+    // method generic arguments of the target method. For example,
+    // <code>extern static void Method1&lt;T1, T2&gt;(Class1&lt;T1&gt; @this)</code>
+    // can be used to call <code>Class1&lt;T1&gt;.Method1&lt;T2&gt;()</code>. The generic constraints of the
+    // <code>extern static</code> method must match generic constraints of the target type, field or method.
+
+    /// <summary>
+    /// Instantiates an <see cref="ILAccessAttribute"/> providing access to a member of kind <see cref="ILAccessorKind"/>.
+    /// </summary>
+    /// <param name="kind">The kind of the target to which access is provided.</param>
+    public ILAccessAttribute(ILAccessorKind kind)
+        => Kind = kind;
+
+    /// <summary>
+    /// Gets the kind of member to which access is provided.
+    /// </summary>
+    public ILAccessorKind Kind { get; }
+
+    /// <summary>
+    /// Gets or sets the name of the member to which access is provided.
+    /// </summary>
+    /// <remarks>
+    /// The name defaults to the annotated method name if not specified.
+    /// The name must be unset/<code>null</code> for <see cref="ILAccessorKind.Constructor"/>.
+    /// </remarks>
+    public string? Name { get; set; }
 }
