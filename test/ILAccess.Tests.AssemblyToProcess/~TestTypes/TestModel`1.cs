d14a0a058356bbf5d3108e1a3374f7ed6197e8c1
huoshan12345 <huoshan12345@hotmail.com>
2025-10-28 19:43:44 -0230

    refine test cases

--- test/ILAccess.Tests.AssemblyToProcess/~TestTypes/TestModel`1.cs
+++ test/ILAccess.Tests.AssemblyToProcess/~TestTypes/TestModel`1.cs
@@ -41,9 +41,45 @@ partial class TestModelAccessors
 
     [ILAccessor(ILAccessorKind.Constructor)]
     public static extern TestModel<T> Ctor<T>(int i, string s, ref double rf);
+
+    [ILAccessor(ILAccessorKind.StaticField, Name = "PrivateStaticField")]
+    public static extern T PrivateStaticField<T>(this TestModel<T>? c);
+
+    [ILAccessor(ILAccessorKind.StaticField, Name = "PublicStaticField")]
+    public static extern T PublicStaticField<T>(this TestModel<T>? c);
+
+    [ILAccessor(ILAccessorKind.Field, Name = "PrivateField")]
+    public static extern T PrivateField<T>(this TestModel<T> c);
+
+    [ILAccessor(ILAccessorKind.Field, Name = "PublicField")]
+    public static extern T PublicField<T>(this TestModel<T> c);
+
+    [ILAccessor(ILAccessorKind.StaticField, Name = "PrivateStaticField")]
+    public static extern ref T RefPrivateStaticField<T>(this TestModel<T>? c);
+
+    [ILAccessor(ILAccessorKind.StaticField, Name = "PublicStaticField")]
+    public static extern ref T RefPublicStaticField<T>(this TestModel<T>? c);
+
+    [ILAccessor(ILAccessorKind.Field, Name = "PrivateField")]
+    public static extern ref T RefPrivateField<T>(this TestModel<T> c);
+
+    [ILAccessor(ILAccessorKind.Field, Name = "PublicField")]
+    public static extern ref T RefPublicField<T>(this TestModel<T> c);
+
+    [ILAccessor(ILAccessorKind.Method, Name = ".ctor")]
+    public static extern void CtorAsMethod<T>(this TestModel<T> c, int i, string s, ref double rf);
+
+    public static T PrivateStaticField_GenericAccessors<T>(this TestModel<T>? c) => GenericTestModelAccessors<T>.PrivateStaticField(c);
+    public static T PublicStaticField_GenericAccessors<T>(this TestModel<T>? c) => GenericTestModelAccessors<T>.PublicStaticField(c);
+    public static T PrivateField_GenericAccessors<T>(this TestModel<T> c) => GenericTestModelAccessors<T>.PrivateField(c);
+    public static T PublicField_GenericAccessors<T>(this TestModel<T> c) => GenericTestModelAccessors<T>.PublicField(c);
+    public static ref T RefPrivateStaticField_GenericAccessors<T>(this TestModel<T>? c) => ref GenericTestModelAccessors<T>.RefPrivateStaticField(c);
+    public static ref T RefPublicStaticField_GenericAccessors<T>(this TestModel<T>? c) => ref GenericTestModelAccessors<T>.RefPublicStaticField(c);
+    public static ref T RefPrivateField_GenericAccessors<T>(this TestModel<T> c) => ref GenericTestModelAccessors<T>.RefPrivateField(c);
+    public static ref T RefPublicField_GenericAccessors<T>(this TestModel<T> c) => ref GenericTestModelAccessors<T>.RefPublicField(c);
 }
 
-public static class GenericAccessors<T>
+public static class GenericTestModelAccessors<T>
 {
     [ILAccessor(ILAccessorKind.StaticField, Name = "PrivateStaticField")]
     public static extern T PrivateStaticField(TestModel<T>? c);
@@ -77,76 +113,4 @@ public static class GenericAccessors<T>
 
     [ILAccessor(ILAccessorKind.Method, Name = ".ctor")]
     public static extern void CtorAsMethod(TestModel<T> c, int i, string s, ref double rf);
-}
-
-public static partial class GenericTestModelExtensions
-{
-    [ILAccessor(ILAccessorKind.StaticField, Name = "PrivateStaticField")]
-    public static extern T PrivateStaticField<T>(this TestModel<T>? c);
-
-    [ILAccessor(ILAccessorKind.StaticField, Name = "PublicStaticField")]
-    public static extern T PublicStaticField<T>(this TestModel<T>? c);
-
-    [ILAccessor(ILAccessorKind.Field, Name = "PrivateField")]
-    public static extern T PrivateField<T>(this TestModel<T> c);
-
-    [ILAccessor(ILAccessorKind.Field, Name = "PublicField")]
-    public static extern T PublicField<T>(this TestModel<T> c);
-
-    [ILAccessor(ILAccessorKind.StaticField, Name = "PrivateStaticField")]
-    public static extern ref T RefPrivateStaticField<T>(this TestModel<T>? c);
-
-    [ILAccessor(ILAccessorKind.StaticField, Name = "PublicStaticField")]
-    public static extern ref T RefPublicStaticField<T>(this TestModel<T>? c);
-
-    [ILAccessor(ILAccessorKind.Field, Name = "PrivateField")]
-    public static extern ref T RefPrivateField<T>(this TestModel<T> c);
-
-    [ILAccessor(ILAccessorKind.Field, Name = "PublicField")]
-    public static extern ref T RefPublicField<T>(this TestModel<T> c);
-
-    [ILAccessor(ILAccessorKind.Method, Name = ".ctor")]
-    public static extern void CtorAsMethod<T>(this TestModel<T> c, int i, string s, ref double rf);
-}
-
-public static partial class GenericTestModelExtensions
-{
-    public static T PrivateStaticField_GenericAccessors<T>(this TestModel<T>? c) => GenericAccessors<T>.PrivateStaticField(c);
-    public static T PublicStaticField_GenericAccessors<T>(this TestModel<T>? c) => GenericAccessors<T>.PublicStaticField(c);
-    public static T PrivateField_GenericAccessors<T>(this TestModel<T> c) => GenericAccessors<T>.PrivateField(c);
-    public static T PublicField_GenericAccessors<T>(this TestModel<T> c) => GenericAccessors<T>.PublicField(c);
-    public static ref T RefPrivateStaticField_GenericAccessors<T>(this TestModel<T>? c) => ref GenericAccessors<T>.RefPrivateStaticField(c);
-    public static ref T RefPublicStaticField_GenericAccessors<T>(this TestModel<T>? c) => ref GenericAccessors<T>.RefPublicStaticField(c);
-    public static ref T RefPrivateField_GenericAccessors<T>(this TestModel<T> c) => ref GenericAccessors<T>.RefPrivateField(c);
-    public static ref T RefPublicField_GenericAccessors<T>(this TestModel<T> c) => ref GenericAccessors<T>.RefPublicField(c);
-}
-
-public static class ListAccessors
-{
-    [ILAccessor(ILAccessorKind.Field, Name = "_items")]
-    public static extern ref T[] Items<T>(this List<T> obj);
-
-    [ILAccessor(ILAccessorKind.StaticMethod, Name = "IsCompatibleObject")]
-    public static extern bool IsCompatibleObject<T>(this List<T> obj, object? value);
-
-    [ILAccessor(ILAccessorKind.Constructor)]
-    public static extern List<T> Ctor<T>();
-
-    [ILAccessor(ILAccessorKind.Constructor)]
-    public static extern List<T> Ctor<T>(int capacity);
-}
-
-public static class GenericListAccessors<T>
-{
-    [ILAccessor(ILAccessorKind.Field, Name = "_items")]
-    public static extern ref T[] Items(List<T> obj);
-
-    [ILAccessor(ILAccessorKind.StaticMethod, Name = "IsCompatibleObject")]
-    public static extern bool IsCompatibleObject(List<T> obj, object? value);
-
-    [ILAccessor(ILAccessorKind.Constructor)]
-    public static extern List<T> Ctor();
-
-    [ILAccessor(ILAccessorKind.Constructor)]
-    public static extern List<T> Ctor(int capacity);
 }
