df87e4fa63e5cc560e6392317eacd6cde729eaa5
huoshan12345 <huoshan12345@hotmail.com>
2026-08-18 04:27:37 -0230

    Migrate from VSTest to Microsoft.Testing.Platform (MTP)

--- test/ILAccess.Tests/Properties/AssemblyInfo.cs
+++ test/ILAccess.Tests/Properties/AssemblyInfo.cs
@@ -1,1 +1,5 @@
-﻿[assembly: CollectionBehavior(DisableTestParallelization = true)]
+﻿using Xunit.Sdk;
+using Xunit.v3;
+
+// need to disable parallelization for the entire assembly because some tests are not thread-safe like getting/setting data for static fields and properties
+[assembly: Parallelization(Mode = ParallelMode.None)]
