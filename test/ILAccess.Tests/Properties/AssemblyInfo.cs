06031843bdd9add512eac30385471be3a03d9d0b
huoshan12345 <huoshan12345@hotmail.com>
2026-08-20 19:29:03 -0230

    refine files

--- test/ILAccess.Tests/Properties/AssemblyInfo.cs
+++
@@ -1,5 +0,0 @@
-﻿using Xunit.Sdk;
-using Xunit.v3;
-
-// need to disable parallelization for the entire assembly because some tests are not thread-safe like getting/setting data for static fields and properties
-[assembly: Parallelization(Mode = ParallelMode.None)]
